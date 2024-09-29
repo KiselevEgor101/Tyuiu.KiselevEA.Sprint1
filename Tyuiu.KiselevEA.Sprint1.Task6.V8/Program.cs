e6621239c5a53b04e64e1edee1e0f7e6dda34af0
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-09-29 18:49:32 +0500

    Task 6

--- Tyuiu.KiselevEA.Sprint1.Task6.V8/Program.cs
+++ Tyuiu.KiselevEA.Sprint1.Task6.V8/Program.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.KiselevEA.Sprint1.Task6.V8
             Console.WriteLine("* Тема: Работа со строками класс String                                   *");
             Console.WriteLine("* Задание #6                                                              *");
             Console.WriteLine("* Вариант #8                                                              *");
-            Console.WriteLine("* Выполнил: Киселев Егор Алексеевич | СМАРТб-24-1                         *");
+            Console.WriteLine("* Выполнил: Киселев Егор Алексеевич | СМАРТб-24-1                        .*");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу: пользователь вводит текст. Напечатать все слова,    *");
